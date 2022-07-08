cf4b8d293c799ab2554b2298356f560b3073bc25
Uno Kim <djkehh@gmail.com>
2022-07-08 16:26:20 +0900

    컨트롤러와 서비스 추가
    간단한 엔드포인트를 구성하고, 서비스코드를 호출해서
    인사하는 api를 구현함
    
    DI 를 여기서 설명했고, 생성자 방식으로 주입함

---
+++ src/main/java/com/example/fastcampuspractice/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.example.fastcampuspractice.controller;
+
+import com.example.fastcampuspractice.service.HelloService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class MainController {
+
+    private final HelloService helloService;
+
+    @GetMapping("/api/main")
+    public String main(@RequestParam String name) {
+        return helloService.sayHello(name);
+    }
+
+}
