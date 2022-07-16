634edb802dd7ab546af2be4eff4d789587c84deb
Uno Kim <djkehh@gmail.com>
2022-07-17 02:07:02 +0900

    메인 컨트롤러에 검증 로직 테스트용 핸들러 메소드 추가
    핸들러 메소드는 데이터를 Request body 로 받고,
    이는 자바 객체로 표현되며
    애노테이션으로 멤버 필드마다 검증이 들어감

--- src/main/java/com/example/fastcampuspractice/controller/MainController.java
+++ src/main/java/com/example/fastcampuspractice/controller/MainController.java
@@ -2,9 +2,11 @@ package com.example.fastcampuspractice.controller;
 
 import com.example.fastcampuspractice.service.HelloService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,5 +19,16 @@ public class MainController {
         return helloServiceImpl.sayHello(name);
     }
 
+    @PostMapping("/api/validation")
+    public String validation(@Valid @RequestBody UnoRequest request) {
+        return "good";
+    }
+
+
+    public record UnoRequest(
+            @NotBlank String name,
+            @Min(10) Integer age
+    ) {
+    }
 
 }
