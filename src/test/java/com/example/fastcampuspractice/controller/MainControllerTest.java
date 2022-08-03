6f139bbadddb1a2253081109d3f03e8f4eb06331
Uno Kim <djkehh@gmail.com>
2022-08-03 17:34:23 +0900

    #12 - @SpringBootTest 로 컨트롤러 테스트 하는 방법 설명
    `@SpringBootTest` + `@AutoConfigureMockMvc` 로
    `MockMvc` 빈을 주입 받아 컨트롤러 테스트 하는 기법을 소개
    
    * TODO: 이 테스트는 실제 컨트롤러의 기능을 모두 반영하지 않아서, 테스트 실패함

---
+++ src/test/java/com/example/fastcampuspractice/controller/MainControllerTest.java
@@ -0,0 +1,35 @@
+package com.example.fastcampuspractice.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class MainControllerTest {
+
+    private final MockMvc mvc;
+
+    public MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    void givenNothing_whenRequestingMainPage_thenReturnsMainIndexPage() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/main"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("main/index"));
+
+    }
+
+}
