fdfff583e683ffd6c88c2873fd8b2047c296625d
Uno Kim <djkehh@gmail.com>
2022-08-03 17:39:01 +0900

    #12 - @WebMvcTest 로 더 가볍게 컨트롤러 테스트를 하는 방법
    보다 가볍게 스프링 테스트를 할 수 있도록 하는
    스프링 슬라이스 테스트 기법을 소개
    문법만 가벼워지는게 아니라, 내부적으로 스프링 컨테이너가
    로딩할 빈을 컨트롤러 관련 빈으로 제한함으로써
    실제로 테스트가 좀 더 가볍게 동작하도록 함

--- src/test/java/com/example/fastcampuspractice/controller/MainControllerTest.java
+++ src/test/java/com/example/fastcampuspractice/controller/MainControllerTest.java
@@ -2,16 +2,14 @@ package com.example.fastcampuspractice.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@AutoConfigureMockMvc
-@SpringBootTest
+@WebMvcTest
 class MainControllerTest {
 
     private final MockMvc mvc;
