d05aa4fbde7cb806616bdbf12a029962fa26fb3c
Uno Kim <djkehh@gmail.com>
2022-08-22 16:12:55 +0900

    #26 - 과목 예제 sql 데이터 작성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,11 @@ insert into student (name, age, grade, created_at, created_by, modified_at, modi
 ('uno5', 38, 'D', now(), 'uno', now(), 'uno'),
 ('uno6', 48, 'A', now(), 'uno', now(), 'uno')
 ;
+
+
+insert into subject (student_id, name, created_at, created_by, modified_at, modified_by) values
+(1, '수학', now(), 'uno', now(), 'uno'),
+(2, '영어', now(), 'uno', now(), 'uno'),
+(2, '역사', now(), 'uno', now(), 'uno'),
+(3, '체육', now(), 'uno', now(), 'uno')
+;
